4aa4a58551c004651eb2d52b075e983e8f216ec9
Josh Birch <joshuabirch@protonmail.com>
2025-06-04 08:04:06 +0100

    Add clock to game loop

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ def main():
     print('Screen width:', SCREEN_WIDTH)
     print('Screen height:', SCREEN_HEIGHT)
 
+    # init
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # FPS
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # game loop
     while(True):
 
         # makes windows close button work
@@ -18,6 +24,7 @@ def main():
                 
         screen.fill('black')
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
