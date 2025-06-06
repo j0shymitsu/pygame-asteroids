096b067e6471e519140dd704c262eb2cda7ffa85
Josh Birch <joshuabirch@protonmail.com>
2025-06-06 08:37:24 +0100

    Implement groups to consolidate objects

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
     # init
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = updatable, drawable
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     # FPS
@@ -24,14 +27,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
+        updatable.update(dt)
         screen.fill('black')
-        player.draw(screen)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-
-        
-
 
 if __name__ == "__main__":
     main()
