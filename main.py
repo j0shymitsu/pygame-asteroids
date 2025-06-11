f896b28080be08d51106ed06ff45043a06847120
Josh Birch <joshuabirch@protonmail.com>
2025-06-11 09:01:26 +0100

    Add shooting mechanics and shot class implementation; still need collision detection and slowing down

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from constants import *
 from player import *
 from asteroid import *
 from asteroidfield import *
+from shot import Shot
 
 def main():
     print('Starting Asteroids!')
@@ -16,9 +17,11 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = updatable, drawable
     Asteroid.containers = asteroids, updatable, drawable
     AsteroidField.containers = updatable
+    Shot.containers = updatable, drawable
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroidfield = AsteroidField()
