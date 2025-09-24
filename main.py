7cddfc59ec24ece8b4a694f0e38c75e12f1eaba6
Josh Birch <joshuabirch@protonmail.com>
2025-09-24 15:18:15 +0100

    Begin score tracking; displays in console, blit won't update

--- main.py
+++ main.py
@@ -26,6 +26,10 @@ def main():
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroidfield = AsteroidField()
 
+    player_score = 0
+    font = pygame.font.SysFont('ubuntu', 45)
+    score_surface = pygame.font.Font.render(font, str(player_score), True, (0, 255, 0))
+
     # FPS
     clock = pygame.time.Clock()
     dt = 0
@@ -49,14 +53,20 @@ def main():
                 if shot.collide(asteroid):
                     shot.kill()
                     asteroid.split()
-
+                    player_score += 1
+                    print(player_score)         # Temporary
+                    
         screen.fill('black')
+        screen.blit(score_surface, (10, 10))    # Not updating
 
         for item in drawable:
             item.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+
+
+
 
 if __name__ == "__main__":
     main()
